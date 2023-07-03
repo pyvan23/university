0ffa4c4475ec43af7efc918cfa17585f40b5cf0d
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-07-03 14:16:46 -0300

    Curso migrado y tabla

--- university-api-backend/Models/DataModels/Curso.cs
+++ university-api-backend/Models/DataModels/Curso.cs
@@ -1,6 +1,32 @@
-﻿namespace university_api_backend
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace university_api_backend.Models.DataModels
 {
-    public class Curso
+    public enum NivelCurso
     {
+        Basico,
+        Intermedio,
+        Avanzado
+    }
+
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+
+        public string DescripcionLarga { get; set; } = string.Empty;
+
+        public string PublicoObjetivo { get; set; } = string.Empty;
+
+        public string Objetivos { get; set; } = string.Empty;
+
+        public string Requisitos { get; set; } = string.Empty;
+
+        [Required]
+        public NivelCurso Nivel { get; set; }
     }
 }
+
